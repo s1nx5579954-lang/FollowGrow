123849bd266d953ca3b732bb4e986d9b81621295
shota <s1nX5579954@gmail.com>
2026-08-23 18:03:07 +0900

    変更内容

--- OneDay_todo/urls.py
+++ OneDay_todo/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='oneday_index'),
-    path('list/create/', views.list_create, name='list_create'),
-    path('list/<int:list_id>/delete/', views.list_delete, name='list_delete'), 
-    path('list/<int:list_id>/task/add/', views.task_add, name='task_add'),
-    path('task/<int:task_id>/delete/', views.task_delete, name='task_delete'),
-    path('task/<int:task_id>/toggle/', views.task_toggle, name='task_toggle'),
+    path('list/create/', views.list_create, name='oneday_list_create'),
+    path('list/<int:list_id>/delete/', views.list_delete, name='oneday_list_delete'),
+    path('list/<int:list_id>/task/add/', views.task_add, name='oneday_task_add'),
+    path('task/<int:task_id>/delete/', views.task_delete, name='oneday_task_delete'),
+    path('task/<int:task_id>/toggle/', views.task_toggle, name='oneday_task_toggle'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -126,3 +126,7 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 LOGIN_URL = 'login'
+
+#メディア
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
